e63a8f99cbd17abd6cc35084d80e5f754bf80058
jadessu <junioradessu@gmail.com>
2021-08-02 20:26:52 -0500

    add paths to switches

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react"
-import {BrowserRouter as Router} from "react-router-dom"
+import {BrowserRouter as Router, Switch, Route} from "react-router-dom"
 import './App.css';
 import Navbar from "./components/Navbar"
 
@@ -7,8 +7,24 @@ import Navbar from "./components/Navbar"
 function App() {
   return (
     <Router>
-    
     <Navbar />
+    <Switch>
+    <Route path="/" exact component={Home} />
+    <Route path="/action" exact component={Action} />
+    <Route path="/animation" exact component={Animation} />
+    <Route path="/categories" exact component={Categories} />
+    <Route path="/comedy" exact component={Comedy} />
+    <Route path="/drama" exact component={Drama} />
+    <Route path="/fantasy" exact component={Fantasy} />
+    <Route path="/horror" exact component={Horror} />
+    <Route path="/romance" exact component={Romance} />
+    <Route path="/toprated" exact component={TopRated} />
+    <Route path="/upcoming" exact component={Upcoming} />
+
+
+
+
+    </Switch>
     </Router>
   );
 }
